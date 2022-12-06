742b2df269d979abb21489d3479b292bd76a5350
kmww <kmw9708@gmail.com>
2022-12-06 18:17:25 +0900

    vuex

---
+++ vue_vuex/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increaseCount">increase!</button>
+  <Hello />
+  <World />
+  <button @click="fetchTodo">update</button>
+</template>
+
+<script>
+import Hello from "~/components/Hello";
+import World from "~/components/World";
+
+export default {
+  components: {
+    Hello,
+    World,
+  },
+  data() {
+    return {
+      msg: "Hello Vue",
+    };
+  },
+  methods: {
+    increaseCount() {
+      this.$store.commit("increaseCount");
+    },
+    fetchTodo() {
+      this.$store.dispatch("fetchTodo");
+    },
+  },
+};
+</script>
---
+++ vue_vuex/src/store/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state() {
+    return {
+      msg: "Hello Vue!",
+      count: 1,
+    };
+  },
+  getters: {
+    reversedMsg(state) {
+      return state.msg.split("").reverse().join("");
+    },
+  },
+  mutations: {
+    increaseCount(state) {
+      state.count += 1;
+    },
+    updateMsg(state, newMsg) {
+      state.msg = newMsg;
+    },
+  },
+  actions: {
+    // context => state, getters, commit, dispatch
+    async fetchTodo({ commit }) {
+      const todo = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      ).then((res) => res.json());
+      console.log(todo);
+      commit("updateMsg", todo.title);
+    },
+  },
+});
