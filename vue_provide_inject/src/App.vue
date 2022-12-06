9f174cd0b77f680bc55b62331bc818d710cd1ed7
kmww <kmw9708@gmail.com>
2022-12-06 17:29:33 +0900

    vue_provide&inject

---
+++ vue_provide_inject/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 @click="msg += '!!'">{{ msg }}</h1>
+  <Parent />
+</template>
+
+<script>
+import Parent from "~/components/Parent";
+import { computed } from "vue";
+
+export default {
+  components: {
+    Parent,
+  },
+  // provide는 data 위에 작성
+  provide() {
+    return {
+      msg: computed(() => this.msg),
+    };
+  },
+  data() {
+    return {
+      msg: "Hello Vue",
+    };
+  },
+};
+</script>
